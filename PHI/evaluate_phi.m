bcdfac74b411cf38c85ad19fdfbfdb87745b50c1
CyberZHG <cyberzhg@gmail.com>
2014-12-12 15:55:48 +0800

    Evaluate and test faster.
    Signed-off-by: CyberZHG <cyberzhg@gmail.com>

--- PHI/evaluate_phi.m
+++ PHI/evaluate_phi.m
@@ -1,42 +1,42 @@
 function Precision = evaluate_phi(Config, Path)
     disp('Evaluating...');
     Num = Config.EvaluateNum;
-    Precision = zeros([Num, 1]);
-    Files = dir(Path);
-    FileNum = 0;
-    for k = 1 : length(Files)
-        FileName = Files(k).name;
-        if length(FileName) > 6 && strcmp(FileName(end - 6 : end), '.result')
-            FileNum = FileNum + 1;
-            Temp = regexp(FileName, '\.', 'split');
-            Temp = Temp{1};
-            Temp = regexp(Temp, '_', 'split');
-            Category = '';
-            for i = 1 : length(Temp) - 1
-                if i > 1
-                    Category = [Category '_'];
-                end
-                Category = [Category Temp{i}];
+    Precisions = cell([Config.Task.QueryNum, 1]);
+    QueryName = Config.Task.QueryName;
+    parfor k = 1 : Config.Task.QueryNum
+        FileName = [QueryName{k} '.result'];
+        Temp = QueryName{k};
+        Temp = regexp(Temp, '_', 'split');
+        Category = '';
+        for i = 1 : length(Temp) - 1
+            if i > 1
+                Category = [Category '_'];
             end
-            File = fopen([Path '/' FileName], 'r');
-            Match = 0;
-            for i = 1 : Num
-                Temp = read_string(File);
-                Temp = regexp(Temp, '_', 'split');
-                Cat = '';
-                for j = 1 : length(Temp) - 1
-                    if j > 1
-                        Cat = [Cat '_'];
-                    end
-                    Cat = [Cat Temp{j}];
-                end
-                if strcmp(Cat, Category)
-                    Match = Match + 1;
-                end
-                Precision(i) = Precision(i) + Match / i;
-            end
-            fclose(File);
+            Category = [Category Temp{i}];
         end
+        File = fopen([Path '/' FileName], 'r');
+        Match = 0;
+        Precisions{k} = zeros([Num, 1]);
+        for i = 1 : Num
+            Temp = read_string(File);
+            Temp = regexp(Temp, '_', 'split');
+            Cat = '';
+            for j = 1 : length(Temp) - 1
+                if j > 1
+                    Cat = [Cat '_'];
+                end
+                Cat = [Cat Temp{j}];
+            end
+            if strcmp(Cat, Category)
+                Match = Match + 1;
+            end
+            Precisions{k}(i) = Precisions{k}(i) + Match / i;
+        end
+        fclose(File);
     end
-    Precision = Precision / FileNum;
+    Precision = zeros([Num, 1]);
+    for k = 1 : Config.Task.QueryNum
+        Precision = Precision + Precisions{k};
+    end
+    Precision = Precision / Config.Task.QueryNum;
 end
--- PHI/test_phi.m
+++ PHI/test_phi.m
@@ -9,36 +9,45 @@ function Path = test_phi(Config)
     FeatureConfig.BitNum = Config.BitNum;
     Md5 = md5(FeatureConfig);
     
-    disp('Testing...');
-    
     if ~exist('Results', 'dir')
         mkdir('Results');
     end
     Path = ['Results/' Md5];
-    if exist(Path, 'dir')
-        return;
-    else
+    if ~exist(Path, 'dir')
         mkdir(Path);
     end
     
-    DatasetBits = cell([Config.Task.DatasetNum, 1]);
+    disp('Compress training...');
+    
+    RowNum = floor((Config.Height - Config.PatchLength) / Config.ShiftLength) + 1;
+    ColNum = floor((Config.Width - Config.PatchLength) / Config.ShiftLength) + 1;
+    PatchNum = RowNum * ColNum;
+    CellBits = cell([Config.Task.DatasetNum, 1]);
     parfor k = 1 : Config.Task.DatasetNum
         Feature = generate_feature(Config, Config.Task.DatasetPath{k});
-        DatasetBits{k} = compressSH(Feature, Config.SHparam);
+        CellBits{k} = compressSH(Feature, Config.SHparam);
+    end
+    DatasetBits = zeros([PatchNum * Config.Task.DatasetNum, Config.BitNum / 8], 'uint8');
+    for k = 1 : Config.Task.DatasetNum
+        DatasetBits((k - 1) * PatchNum + 1 : k * PatchNum, :) = CellBits{k};
     end
     
+    disp('Testing...');
+    QueryName = Config.Task.QueryName;
     parfor k = 1 : Config.Task.QueryNum
+        if exist([Path '/' QueryName{k} '.result'], 'file')
+            continue;
+        end
         Feature = generate_feature(Config, Config.Task.QueryPath{k});
         Bits = compressSH(Feature, Config.SHparam);
+        Dists = hammingDist(Bits, DatasetBits);
         Dist = zeros([Config.Task.DatasetNum, 1]);
         for i = 1 : Config.Task.DatasetNum
-            for j = 1 : size(Bits, 1)
-                Dist(i) = Dist(i) + hammingDist(Bits(j), DatasetBits{i}(j));
-            end
+            Dist(i) = trace(Dists(:, (i - 1) * PatchNum + 1 : i * PatchNum));
         end
         [~, Index] = sort(Dist);
         File = fopen([Path '/' Config.Task.QueryName{k} '.result'], 'w');
-        for i = 1 : Config.Task.DatasetNum
+        for i = 1 : Config.EvaluateNum
             fprintf(File, '%s\n', Config.Task.DatasetName{Index(i)});
         end
         fclose(File);
